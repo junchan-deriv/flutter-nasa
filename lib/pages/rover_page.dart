a52b89ced0689c24df418cf5e011d0fbbff8fb6e
Dharisini <dharisini@besquare.com.my>
2022-08-08 17:07:54 +0800

    added images

--- lib/pages/rover_page.dart
+++ lib/pages/rover_page.dart
@@ -10,17 +10,35 @@ class RoverPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: const Text(
-            "Mars Rover",
-            style: TextStyle(
-                fontFamily: 'ChakraPetch',
-                fontSize: 40,
-                fontWeight: FontWeight.w500),
+        appBar: AppBar(
+          title: Center(
+            child: const Text(
+              "Mars Rover",
+              style: TextStyle(
+                  fontFamily: 'ChakraPetch',
+                  fontSize: 40,
+                  fontWeight: FontWeight.w500),
+            ),
           ),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(children: [
+            Container(
+              height: 90.0,
+              width: 300.0,
+              color: Colors.red,
+            ),
+            Container(
+              height: 90.0,
+              width: 300.0,
+              color: Color.fromARGB(255, 145, 84, 80),
+            ),
+            Container(
+              height: 90.0,
+              width: 300.0,
+              color: Colors.red,
+            ),
+          ]),
+        ));
   }
 }
