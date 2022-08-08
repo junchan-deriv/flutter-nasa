f94d8a86b5ab0f5b44ee911f33707c88618885af
Jia Jun Ngeo <jia.jun@besquare.com.my>
2022-08-08 22:50:14 +0800

    added missing page parameter

--- lib/states/nasa_photo.dart
+++ lib/states/nasa_photo.dart
@@ -3,9 +3,25 @@ import 'package:flutter_nasa/models/nasa_rover_photos.dart';
 import 'package:flutter_nasa/services/nasa.dart';
 import 'package:flutter_nasa/states/loader_base.dart';
 
+class _NasaPhotoLoaderTag {
+  final String rover;
+  final int sol;
+  final RoverCamera? camera;
+  int page;
+  _NasaPhotoLoaderTag(
+      {required this.rover,
+      required this.sol,
+      required this.camera,
+      required this.page});
+}
+
 class NasaPhotoLoaderCubit extends LoaderCubitBase<NasaRoverPhotos> {
-  void fetchNasaRoverPhotos(String rover, int sol, [RoverCamera? camera]) {
+  void fetchNasaRoverPhotos(String rover, int sol,
+      {RoverCamera? camera, int page = 1}) {
     //alias to internal call
-    loadStuffsInternal(NasaService.getRoverPhotos(rover, sol, camera), rover);
+    loadStuffsInternal(
+        NasaService.getRoverPhotos(rover, sol, camera, page),
+        _NasaPhotoLoaderTag(
+            camera: camera, rover: rover, sol: sol, page: page));
   }
 }
